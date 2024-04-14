0dbf0d3c147a0556aaba2d931c8c8da926164ec3
Tomas Climente <tomas.climente@kavak.com>
2024-04-14 18:23:17 -0300

    echo

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    model: str
+    messages: list = []
+    temperature: float
 
 
 @app.get("/")
@@ -13,3 +19,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/v1/chat/completions")
+def chat_openai(item: Item):
+    return item
