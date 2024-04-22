bd0b8fc9aeae5d5769ade2d2c829c9aa276e5a4a
Tomas Climente <tomas.climente@kavak.com>
2024-04-22 20:29:42 -0300

    agrego canchas

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ import os
 
 app = FastAPI()
 
-id = os.environ.get("ID_PLAZA")
+id = os.environ.get('ID_PLAZA')
 url = f'https://alquilatucancha.com/api/v3/availability/sportclubs/{id}?date='
-id_cancha = os.environ.get("ID_CANCHA")
+id_cancha = '3891'
+id_cancha61 = '3892'
+id_cancha62 = '3893'
 
 class Choice(BaseModel):
     hour: str
-
-class Slots(BaseModel):
+    
+class Stadium(BaseModel):
+    name: str
     choices: List[Choice] = []
     
     def add_choice(self, choice: Choice):
         self.choices.append(choice)
+
+class Slots(BaseModel):
+    stadiums: List[Stadium] = []
+    
+    def add_stadium(self, stadium: Stadium):
+        self.stadiums.append(stadium)
 
 @app.get("/health")
 def read_root():
@@ -39,17 +48,37 @@ def chat_openai(day: str):
         
         dt_comparacion = parser.parse(match_time)
     
-        # Acceder al contenido de la respuesta
         content = response.json()
         if 'available_courts' in content:
             for x in content['available_courts']:
                 if x['id'] == id_cancha:
+                    print(x)
                     if 'available_slots' in x:
+                        stadium = Stadium(name="F5")
                         for a in x['available_slots']:
                             dt_actual = parser.parse(a['start'])
                             if dt_actual >= dt_comparacion:
-                                slots.add_choice(Choice(hour=a['start']))
+                                print(a['start'])
+                                stadium.add_choice(Choice(hour=a['start']))
+                        print(stadium)
+                        slots.add_stadium(stadium)
+                elif x['id'] == id_cancha61:
+                    if 'available_slots' in x:
+                        stadium = Stadium(name="F6 1")
+                        for a in x['available_slots']:
+                            dt_actual = parser.parse(a['start'])
+                            if dt_actual >= dt_comparacion:
+                                stadium.add_choice(Choice(hour=a['start']))
+                        slots.add_stadium(stadium)
+                elif x['id'] == id_cancha62:
+                    if 'available_slots' in x:
+                        stadium = Stadium(name="F6 2")
+                        for a in x['available_slots']:
+                            dt_actual = parser.parse(a['start'])
+                            if dt_actual >= dt_comparacion:
+                                stadium.add_choice(Choice(hour=a['start']))
+                        slots.add_stadium(stadium)
     else:
         print("Error:", response.status_code)
-        
+    
     return slots
--- main_bot.py
+++ main_bot.py
@@ -2,21 +2,40 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import os 
+import aiohttp
+from datetime import datetime, timedelta
 
 token = os.environ.get('TELEGRAM')
+
+def next_friday():
+    today = datetime.now()
+    days_until_friday = (4 - today.weekday() + 7) % 7
+    if days_until_friday == 0:
+        days_until_friday = 7
+    next_friday = today + timedelta(days=days_until_friday)
+    return next_friday.date()
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+async def cancha(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    friday = next_friday()
+    url = f'https://custom-llm-rs9d.onrender.com/cancha?day={friday}'
+    async with aiohttp.ClientSession() as session:
+            async with session.get(url, ssl=False) as response:
+                if response.status == 200:
+                    data = await response.text()
+                    await context.bot.send_message(chat_id=update.effective_chat.id, text=data)
+                else:
+                    await context.bot.send_message(chat_id=update.effective_chat.id, text="Error al obtener la información")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('').build()
+    application = ApplicationBuilder().token('6488342940:AAGa07Xk1IcZQOg9XjWjGie4VOQp689toO8').build()
     
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler('cancha', cancha)
     application.add_handler(start_handler)
     
     application.run_polling()
